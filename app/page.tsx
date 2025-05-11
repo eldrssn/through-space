1ea111f3aa285dae0156153213cee687cd96a094
maxkalimullin <maxkalimullin@gmail.com>
2025-05-12 01:56:45 +0500

    fixed build

--- app/page.tsx
+++ app/page.tsx
@@ -1,55 +1,55 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 
-import { Content, Loader } from '@/components'
-import { useGetPlanets } from '@/hooks'
-import { preloadTextures } from '@/modules/space/utils'
-import {
-  AnimatedPlanets,
-  // Space
-} from '@modules'
+// import { Content, Loader } from '@/components'
+// import { useGetPlanets } from '@/hooks'
+// import { preloadTextures } from '@/modules/space/utils'
+// import {
+//   AnimatedPlanets,
+//   // Space
+// } from '@modules'
 
 import * as S from './styled'
 
 export default function Home() {
-  const [loadingProgress, setLoadingProgress] = useState(0)
-  const [showLoader, setShowLoader] = useState(true)
-  const handleAssetsProgress = (progress: number) => {
-    setLoadingProgress(progress)
-  }
+  // const [loadingProgress, setLoadingProgress] = useState(0)
+  // const [showLoader, setShowLoader] = useState(true)
+  // const handleAssetsProgress = (progress: number) => {
+  //   setLoadingProgress(progress)
+  // }
 
-  const { isLoading, planets: planesList } = useGetPlanets()
+  // const { isLoading, planets: planesList } = useGetPlanets()
 
-  const [isAssetsLoaded, setAssetsLoaded] = useState(false)
+  // const [isAssetsLoaded, setAssetsLoaded] = useState(false)
 
-  useEffect(() => {
-    const fakeLoaderTimer = setTimeout(() => {
-      setShowLoader(false)
-    }, 2000)
+  // useEffect(() => {
+  //   const fakeLoaderTimer = setTimeout(() => {
+  //     setShowLoader(false)
+  //   }, 2000)
 
-    const loadTextures = async () => {
-      await preloadTextures((progress) => {
-        handleAssetsProgress(progress)
-      })
-      setAssetsLoaded(true)
-    }
+  //   const loadTextures = async () => {
+  //     await preloadTextures((progress) => {
+  //       handleAssetsProgress(progress)
+  //     })
+  //     setAssetsLoaded(true)
+  //   }
 
-    loadTextures()
+  //   loadTextures()
 
-    return () => {
-      clearTimeout(fakeLoaderTimer)
-    }
-  }, [])
+  //   return () => {
+  //     clearTimeout(fakeLoaderTimer)
+  //   }
+  // }, [])
 
   return (
     <>
       <S.Main>
-        <AnimatedPlanets />
-        <Content />
+        {/* <AnimatedPlanets /> */}
+        {/* <Content /> */}
         {/* {isAssetsLoaded && <Space />} */}
       </S.Main>
-      {(showLoader || !planesList || isLoading || !isAssetsLoaded) && <Loader progress={loadingProgress} />}
+      {/* {(showLoader || !planesList || isLoading || !isAssetsLoaded) && <Loader progress={loadingProgress} />} */}
     </>
   )
 }
