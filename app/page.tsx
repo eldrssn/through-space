9792939a4a49cc6b57a18fbbf3e699afe03d3e94
maxkalimullin <maxkalimullin@gmail.com>
2025-05-12 02:48:50 +0500

    fixed build

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,52 @@
 'use client'
 
-import { Content } from '@/components'
+import { useEffect, useState } from 'react'
+
+import { Content, Loader } from '@/components'
+import { useGetPlanets } from '@/hooks'
 import { AnimatedPlanets } from '@/modules'
+import { preloadTextures } from '@/modules/space/utils'
 
 import * as S from './styled'
 
 export default function Home() {
-  // const [loadingProgress, setLoadingProgress] = useState(0)
-  // const [showLoader, setShowLoader] = useState(true)
-  // const handleAssetsProgress = (progress: number) => {
-  //   setLoadingProgress(progress)
-  // }
+  const [loadingProgress, setLoadingProgress] = useState(0)
+  const [showLoader, setShowLoader] = useState(true)
+  const handleAssetsProgress = (progress: number) => {
+    setLoadingProgress(progress)
+  }
 
-  // const { isLoading, planets: planesList } = useGetPlanets()
+  const { isLoading, planets: planesList } = useGetPlanets()
 
-  // const [isAssetsLoaded, setAssetsLoaded] = useState(false)
+  const [isAssetsLoaded, setAssetsLoaded] = useState(false)
 
-  // useEffect(() => {
-  //   const fakeLoaderTimer = setTimeout(() => {
-  //     setShowLoader(false)
-  //   }, 2000)
+  useEffect(() => {
+    const fakeLoaderTimer = setTimeout(() => {
+      setShowLoader(false)
+    }, 2000)
 
-  //   const loadTextures = async () => {
-  //     await preloadTextures((progress) => {
-  //       handleAssetsProgress(progress)
-  //     })
-  //     setAssetsLoaded(true)
-  //   }
+    const loadTextures = async () => {
+      await preloadTextures((progress) => {
+        handleAssetsProgress(progress)
+      })
+      setAssetsLoaded(true)
+    }
 
-  //   loadTextures()
+    loadTextures()
 
-  //   return () => {
-  //     clearTimeout(fakeLoaderTimer)
-  //   }
-  // }, [])
+    return () => {
+      clearTimeout(fakeLoaderTimer)
+    }
+  }, [])
 
   return (
     <>
       <S.Main>
-        {/* <Header> Together Through the&nbsp;Space</Header> */}
         <AnimatedPlanets />
         <Content />
         {/* {isAssetsLoaded && <Space />} */}
       </S.Main>
-      {/* {(showLoader || !planesList || isLoading || !isAssetsLoaded) && <Loader progress={loadingProgress} />} */}
+      {(showLoader || !planesList || isLoading || !isAssetsLoaded) && <Loader progress={loadingProgress} />}
     </>
   )
 }
